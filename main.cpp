88a8648974e9a272409fb01fbf490e57577f082a
夕輝 豊田 <K022G0030@g.neec.ac.jp>
2023-06-30 01:30:46 +0900

    master

--- main.cpp
+++ main.cpp
@@ -24,17 +24,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// カメラ回転角
 	Vector3 cameraRotate{ 0.26f, 0.0f, 0.0f };
 
-	// 四角形
-	AABB aabb1{
-		-0.5f, -0.5f, -0.25f,
-		0.0f, 0.0f, 0.0f };
-	uint32_t color = WHITE;
-
-	AABB aabb2{
-		0.2f, 0.2f, -0.25f,
-		1.0f, 1.0f, 1.0f
-	};
-
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -64,35 +53,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		// ビューポート行列生成
 		Matrix4x4 viewPortmatrix = MyMath::MakeViewPortMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
 
-		// 立方体同士の当たり判定をとる
-		if (MyCollision::IsCollision(aabb1, aabb2)) {
-			// 色を変える
-			color = RED;
-		}
-		else {
-			// 色を戻す
-			color = WHITE;
-		}
-
-		// 頂点が入れ替わらないように
-		aabb1.min.x = (std::min)(aabb1.min.x, aabb1.max.x);
-		aabb1.max.x = (std::max)(aabb1.min.x, aabb1.max.x);
-
-		aabb1.min.y = (std::min)(aabb1.min.y, aabb1.max.y);
-		aabb1.max.y = (std::max)(aabb1.min.y, aabb1.max.y);
-
-		aabb1.min.z = (std::min)(aabb1.min.z, aabb1.max.z);
-		aabb1.max.z = (std::max)(aabb1.min.z, aabb1.max.z);
-
-		aabb2.min.x = (std::min)(aabb2.min.x, aabb2.max.x);
-		aabb2.max.x = (std::max)(aabb2.min.x, aabb2.max.x);
-
-		aabb2.min.y = (std::min)(aabb2.min.y, aabb2.max.y);
-		aabb2.max.y = (std::max)(aabb2.min.y, aabb2.max.y);
-
-		aabb2.min.z = (std::min)(aabb2.min.z, aabb2.max.z);
-		aabb2.max.z = (std::max)(aabb2.min.z, aabb2.max.z);
-
 		///
 		/// ↑更新処理ここまで
 		///
@@ -103,10 +63,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		// グリッドを描画する
 		MyDebug::DrawGrid(worldViewProjectionMatrix, viewPortmatrix);
-
-		// AABBを描画する
-		MyDebug::DrawAABB(aabb1, worldViewProjectionMatrix, viewPortmatrix, color);
-		MyDebug::DrawAABB(aabb2, worldViewProjectionMatrix, viewPortmatrix, WHITE);
 
 		///
 		/// ↑描画処理ここまで
@@ -123,16 +79,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		ImGui::DragFloat3("cameraTranslate", &cameraTranslate.x, 0.01f);
 		// カメラの回転角をいじる
 		ImGui::DragFloat3("cameraRotate", &cameraRotate.x, 0.01f);
-
-		// AABBの最小値をいじる
-		ImGui::DragFloat3("aabb1.min", &aabb1.min.x, 0.01f);
-		// AABBの最大値をいじる
-		ImGui::DragFloat3("aabb1.max", &aabb1.max.x, 0.01f);
-
-		// AABBの最小値をいじる
-		ImGui::DragFloat3("aabb2.min", &aabb2.min.x, 0.01f);
-		// AABBの最大値をいじる
-		ImGui::DragFloat3("aabb2.max", &aabb2.max.x, 0.01f);
 
 		ImGui::End();
 
